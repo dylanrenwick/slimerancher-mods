7e14a56ed47b284b1a9ee912cbfc1751c4d5848b
skystrikedevteam@gmail.com <skystrikedevteam@gmail.com>
2016-04-16 08:29:15 +0100

    Added ExtendedVacpac VS project

---
+++ SlimeRancher Mods/ExtendedVacpac/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExtendedVacpac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("David Sisco")]
+[assembly: AssemblyProduct("ExtendedVacpac")]
+[assembly: AssemblyCopyright("Copyright © DavidSisco 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("462e90a1-1942-478b-9a95-a8b4365aed3c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SlimeRancher Mods/ExtendedVacpac/SR_Plugin.cs
@@ -0,0 +1,31 @@
+﻿using SR_PluginLoader;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SR_Plugin_Namespace
+{
+
+    // Please note that this project references assemblies located in the SlimeRancher game folder, specifically the default location for the steam version on windows.
+    // If you are getting missing assembly reference errors then you need to correct the locations for those files.
+    public static class SR_Plugin
+    {
+        public static Plugin_Data PLUGIN_INFO = new Plugin_Data()
+        {
+            NAME = "Extended Vacpac",
+            AUTHOR = "GunnerWolf",
+            DESCRIPTION = @"",
+            VERSION = new Plugin_Version(0, 1),
+            UPDATE_METHOD = new Update_Method("", UPDATER_TYPE.NONE),
+            DEPENDENCIES = new List<Plugin_Dependency>() { },
+        };
+
+        public static void Load(GameObject go)
+        {
+            
+        }
+
+        public static void Unload(GameObject go)
+        {
+        }
+    }
+}
